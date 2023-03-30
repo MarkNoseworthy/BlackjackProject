34f0e4a630d3fd9e73cace169665d5c9ef555c9b
MarkNoseworthy <mark.noseworthy10@ed.cna.nl.ca>
2023-03-29 23:16:11 -0230

    Getting started

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+
+def cardDeck():
+    cardSuit = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    cardRank = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
 def main():
-    print("Blackjack Project")
+    print("BLACKJACK")
+    print("Blackjack payout is 3:2")
 
 if __name__ == "__main__":
     main()
